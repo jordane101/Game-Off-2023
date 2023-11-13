6f5393549fac8d27b9753c1c3cdcda7d3fa4809b
Eli Jordan <46537957+jordane101@users.noreply.github.com>
2023-11-12 19:19:14 -0700

    reimplemented player movement
    Added ledge grab functionality, sprinting, removed wall running, climbing

--- Assets/Scripts/LedgeGrabbing.cs
+++ Assets/Scripts/LedgeGrabbing.cs
@@ -17,9 +17,12 @@ public class LedgeGrabbing : MonoBehaviour
     public float minTimeOnLedge;
     private float timeOnLedge;
 
-    private float distanceToLedge;
-
     public bool holding;
+
+    [Header("Ledge Jumping")]
+    public KeyCode jumpKey = KeyCode.Space;
+    public float ledgeJumpForwardForce;
+    public float ledgeJumpUpwardForce;
 
     [Header("Ledge Detection")]
     public float ledgeDetectionLength;
@@ -31,38 +34,70 @@ public class LedgeGrabbing : MonoBehaviour
 
     private RaycastHit ledgeHit;
 
-    private void Update() 
+    [Header("Exiting")]
+    public bool exitingLedge;
+    public float exitLedgeTime;
+    private float exitLedgeTimer;
+
+    private void Update()
     {
         LedgeDetection();
         SubStateMachine();
     }
+
     private void SubStateMachine()
     {
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         float verticalInput = Input.GetAxisRaw("Vertical");
-        bool anyInputKeyPressed = horizontalInput != 0 || verticalInput !=0;
+        bool anyInputKeyPressed = horizontalInput != 0 || verticalInput != 0;
 
+        // SubState 1 - Holding onto ledge
         if (holding)
         {
             FreezeRigidbodyOnLedge();
 
             timeOnLedge += Time.deltaTime;
 
-            if(timeOnLedge > minTimeOnLedge && anyInputKeyPressed) ExitLedgeHold();
+            if (timeOnLedge > minTimeOnLedge && anyInputKeyPressed) ExitLedgeHold();
+
+            if (Input.GetKeyDown(jumpKey)) LedgeJump();
+        }
+
+        // Substate 2 - Exiting Ledge
+        else if (exitingLedge)
+        {
+            if (exitLedgeTimer > 0) exitLedgeTimer -= Time.deltaTime;
+            else exitingLedge = false;
         }
     }
 
     private void LedgeDetection()
     {
         bool ledgeDetected = Physics.SphereCast(transform.position, ledgeSphereCastRadius, cam.forward, out ledgeHit, ledgeDetectionLength, whatIsLedge);
-        if(!ledgeDetected){return;}
+
+        if (!ledgeDetected) return;
+
         float distanceToLedge = Vector3.Distance(transform.position, ledgeHit.transform.position);
 
-        if(ledgeHit.transform == lastLedge) return;
+        if (ledgeHit.transform == lastLedge) return;
 
         if (distanceToLedge < maxLedgeGrabDistance && !holding) EnterLedgeHold();
-
     }
+
+    private void LedgeJump()
+    {
+        ExitLedgeHold();
+
+        Invoke(nameof(DelayedJumpForce), 0.05f);
+    }
+
+    private void DelayedJumpForce()
+    {
+        Vector3 forceToAdd = cam.forward * ledgeJumpForwardForce + orientation.up * ledgeJumpUpwardForce;
+        rb.velocity = Vector3.zero;
+        rb.AddForce(forceToAdd, ForceMode.Impulse);
+    }
+
     private void EnterLedgeHold()
     {
         holding = true;
@@ -76,28 +111,37 @@ public class LedgeGrabbing : MonoBehaviour
         rb.useGravity = false;
         rb.velocity = Vector3.zero;
     }
+
     private void FreezeRigidbodyOnLedge()
     {
         rb.useGravity = false;
-        Vector3 directionToLedge = currLedge.position - transform.position;
 
+        Vector3 directionToLedge = currLedge.position - transform.position;
+        float distanceToLedge = Vector3.Distance(transform.position, currLedge.position);
+
+        // Move player towards ledge
         if(distanceToLedge > 1f)
         {
             if(rb.velocity.magnitude < moveToLedgeSpeed)
-            {
                 rb.AddForce(directionToLedge.normalized * moveToLedgeSpeed * 1000f * Time.deltaTime);
-            }
-        }
-        else
-        {
-            if(!pm.freeze) pm.freeze = true;
-            if(pm.unlimited) pm.unlimited = false;
         }
 
+        // Hold onto ledge
+        else
+        {
+            if (!pm.freeze) pm.freeze = true;
+            if (pm.unlimited) pm.unlimited = false;
+        }
+
+        // Exiting if something goes wrong
         if (distanceToLedge > maxLedgeGrabDistance) ExitLedgeHold();
     }
+
     private void ExitLedgeHold()
     {
+        exitingLedge = true;
+        exitLedgeTimer = exitLedgeTime;
+
         holding = false;
         timeOnLedge = 0f;
 
@@ -107,7 +151,7 @@ public class LedgeGrabbing : MonoBehaviour
         rb.useGravity = true;
 
         StopAllCoroutines();
-        Invoke(nameof(ResetLastLedge),1f);
+        Invoke(nameof(ResetLastLedge), 1f);
     }
 
     private void ResetLastLedge()
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,58 +5,46 @@ using TMPro;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public MovementState state;
-
-    public enum MovementState
-    {
-        freeze,
-        unlimited,
-        walking,
-        sprinting,
-
-
-        vaulting,
-        crouching,
-        sliding,
-        air
-    }
-
-    public bool sliding;
-    public bool crouching;
-
-
-    public bool vaulting;
-
-    public bool freeze;
-    public bool unlimited;
-
-    public bool restricted;
-
-
     [Header("Movement")]
-    public float moveSpeed;
+    private float moveSpeed;
+    private float desiredMoveSpeed;
+    private float lastDesiredMoveSpeed;
+    public float walkSpeed;
+    public float sprintSpeed;
+    public float slideSpeed;
     public float vaultSpeed;
+    public float airMinSpeed;
+
+    public float speedIncreaseMultiplier;
+    public float slopeIncreaseMultiplier;
 
     public float groundDrag;
 
+    [Header("Jumping")]
     public float jumpForce;
     public float jumpCooldown;
     public float airMultiplier;
     bool readyToJump;
 
-    public float coyoteTime;
-    private float coyoteTimeCounter;
-
-    [HideInInspector] public float walkSpeed;
-    [HideInInspector] public float sprintSpeed;
+    [Header("Crouching")]
+    public float crouchSpeed;
+    public float crouchYScale;
+    private float startYScale;
 
     [Header("Keybinds")]
     public KeyCode jumpKey = KeyCode.Space;
+    public KeyCode sprintKey = KeyCode.LeftShift;
+    public KeyCode crouchKey = KeyCode.LeftControl;
 
     [Header("Ground Check")]
     public float playerHeight;
     public LayerMask whatIsGround;
-    bool grounded;
+    public bool grounded;
+
+    [Header("Slope Handling")]
+    public float maxSlopeAngle;
+    private RaycastHit slopeHit;
+    private bool exitingSlope;
 
     public Transform orientation;
 
@@ -67,38 +55,29 @@ public class PlayerMovement : MonoBehaviour
 
     Rigidbody rb;
 
-    private void StateHandler()
+    public MovementState state;
+    public enum MovementState
     {
-        if(freeze)
-        {
-            state = MovementState.freeze;
-            rb.velocity = Vector3.zero;
-        }
-        else if (unlimited)
-        {
-            state = MovementState.unlimited;
-            moveSpeed = 999f;
-            return;
-        }
-        else if (sliding)
-        {
-            state = MovementState.sliding;
-            moveSpeed = 999f;
-            return;
-        }
-        else if (crouching)
-        {
-            state = MovementState.crouching;
-            moveSpeed = 999f;
-            return;
-        }
-        else if (vaulting)
-        {
-            state = MovementState.vaulting;
-            moveSpeed = vaultSpeed;
-            return;
-        }
+        freeze,
+        unlimited,
+        walking,
+        sprinting,
+        vaulting,
+        crouching,
+        sliding,
+        air
     }
+
+    public bool sliding;
+    public bool crouching;
+
+    public bool vaulting;
+
+    public bool freeze;
+    public bool unlimited;
+    
+    public bool restricted;
+
 
     private void Start()
     {
@@ -106,6 +85,8 @@ public class PlayerMovement : MonoBehaviour
         rb.freezeRotation = true;
 
         readyToJump = true;
+
+        startYScale = transform.localScale.y;
     }
 
     private void Update()
@@ -115,18 +96,13 @@ public class PlayerMovement : MonoBehaviour
 
         MyInput();
         SpeedControl();
+        StateHandler();
 
         // handle drag
         if (grounded)
-        {
             rb.drag = groundDrag;
-            coyoteTimeCounter = coyoteTime;
-        }
         else
-        {
-            coyoteTimeCounter -= Time.deltaTime;
             rb.drag = 0;
-        }
     }
 
     private void FixedUpdate()
@@ -140,7 +116,7 @@ public class PlayerMovement : MonoBehaviour
         verticalInput = Input.GetAxisRaw("Vertical");
 
         // when to jump
-        if(Input.GetKey(jumpKey) && readyToJump && (grounded || coyoteTimeCounter > 0f))
+        if (Input.GetKey(jumpKey) && readyToJump && grounded)
         {
             readyToJump = false;
 
@@ -148,45 +124,229 @@ public class PlayerMovement : MonoBehaviour
 
             Invoke(nameof(ResetJump), jumpCooldown);
         }
+
+        // start crouch
+        if (Input.GetKeyDown(crouchKey) && horizontalInput == 0 && verticalInput == 0)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);
+            rb.AddForce(Vector3.down * 5f, ForceMode.Impulse);
+
+            crouching = true;
+        }
+
+        // stop crouch
+        if (Input.GetKeyUp(crouchKey))
+        {
+            transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
+
+            crouching = false;
+        }
+    }
+
+    bool keepMomentum;
+    private void StateHandler()
+    {
+        // Mode - Freeze
+        if (freeze)
+        {
+            state = MovementState.freeze;
+            rb.velocity = Vector3.zero;
+            desiredMoveSpeed = 0f;
+        }
+
+        // Mode - Unlimited
+        else if (unlimited)
+        {
+            state = MovementState.unlimited;
+            desiredMoveSpeed = 999f;
+        }
+
+        // Mode - Vaulting
+        else if (vaulting)
+        {
+            state = MovementState.vaulting;
+            desiredMoveSpeed = vaultSpeed;
+        }
+
+        // Mode - Sliding
+        else if (sliding)
+        {
+            state = MovementState.sliding;
+
+            // increase speed by one every second
+            if (OnSlope() && rb.velocity.y < 0.1f)
+            {
+                desiredMoveSpeed = slideSpeed;
+                keepMomentum = true;
+            }
+
+            else
+                desiredMoveSpeed = sprintSpeed;
+        }
+
+        // Mode - Crouching
+        else if (crouching)
+        {
+            state = MovementState.crouching;
+            desiredMoveSpeed = crouchSpeed;
+        }
+
+        // Mode - Sprinting
+        else if (grounded && Input.GetKey(sprintKey))
+        {
+            state = MovementState.sprinting;
+            desiredMoveSpeed = sprintSpeed;
+        }
+
+        // Mode - Walking
+        else if (grounded)
+        {
+            state = MovementState.walking;
+            desiredMoveSpeed = walkSpeed;
+        }
+
+        // Mode - Air
+        else
+        {
+            state = MovementState.air;
+
+            if (moveSpeed < airMinSpeed)
+                desiredMoveSpeed = airMinSpeed;
+        }
+
+        bool desiredMoveSpeedHasChanged = desiredMoveSpeed != lastDesiredMoveSpeed;
+
+        if (desiredMoveSpeedHasChanged)
+        {
+            if (keepMomentum)
+            {
+                StopAllCoroutines();
+                StartCoroutine(SmoothlyLerpMoveSpeed());
+            }
+            else
+            {
+                moveSpeed = desiredMoveSpeed;
+            }
+        }
+
+        lastDesiredMoveSpeed = desiredMoveSpeed;
+
+        // deactivate keepMomentum
+        if (Mathf.Abs(desiredMoveSpeed - moveSpeed) < 0.1f) keepMomentum = false;
+    }
+
+    private IEnumerator SmoothlyLerpMoveSpeed()
+    {
+        // smoothly lerp movementSpeed to desired value
+        float time = 0;
+        float difference = Mathf.Abs(desiredMoveSpeed - moveSpeed);
+        float startValue = moveSpeed;
+
+        while (time < difference)
+        {
+            moveSpeed = Mathf.Lerp(startValue, desiredMoveSpeed, time / difference);
+
+            if (OnSlope())
+            {
+                float slopeAngle = Vector3.Angle(Vector3.up, slopeHit.normal);
+                float slopeAngleIncrease = 1 + (slopeAngle / 90f);
+
+                time += Time.deltaTime * speedIncreaseMultiplier * slopeIncreaseMultiplier * slopeAngleIncrease;
+            }
+            else
+                time += Time.deltaTime * speedIncreaseMultiplier;
+
+            yield return null;
+        }
+
+        moveSpeed = desiredMoveSpeed;
     }
 
     private void MovePlayer()
     {
         if (restricted) return;
-        
+
         // calculate movement direction
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
+        // on slope
+        if (OnSlope() && !exitingSlope)
+        {
+            rb.AddForce(GetSlopeMoveDirection(moveDirection) * moveSpeed * 20f, ForceMode.Force);
+
+            if (rb.velocity.y > 0)
+                rb.AddForce(Vector3.down * 80f, ForceMode.Force);
+        }
+
         // on ground
-        if(grounded)
+        else if (grounded)
             rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
 
         // in air
-        else if(!grounded)
+        else if (!grounded)
             rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+
     }
 
     private void SpeedControl()
     {
-        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-
-        // limit velocity if needed
-        if(flatVel.magnitude > moveSpeed)
+        // limiting speed on slope
+        if (OnSlope() && !exitingSlope)
         {
-            Vector3 limitedVel = flatVel.normalized * moveSpeed;
-            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+            if (rb.velocity.magnitude > moveSpeed)
+                rb.velocity = rb.velocity.normalized * moveSpeed;
+        }
+
+        // limiting speed on ground or in air
+        else
+        {
+            Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+            // limit velocity if needed
+            if (flatVel.magnitude > moveSpeed)
+            {
+                Vector3 limitedVel = flatVel.normalized * moveSpeed;
+                rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+            }
         }
     }
 
     private void Jump()
     {
+        exitingSlope = true;
+
         // reset y velocity
         rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-        coyoteTimeCounter = 0f;
+
         rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
     }
     private void ResetJump()
     {
         readyToJump = true;
+
+        exitingSlope = false;
+    }
+
+    public bool OnSlope()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight * 0.5f + 0.3f))
+        {
+            float angle = Vector3.Angle(Vector3.up, slopeHit.normal);
+            return angle < maxSlopeAngle && angle != 0;
+        }
+
+        return false;
+    }
+
+    public Vector3 GetSlopeMoveDirection(Vector3 direction)
+    {
+        return Vector3.ProjectOnPlane(direction, slopeHit.normal).normalized;
+    }
+
+
+    public static float Round(float value, int digits)
+    {
+        float mult = Mathf.Pow(10.0f, (float)digits);
+        return Mathf.Round(value * mult) / mult;
     }
 }
